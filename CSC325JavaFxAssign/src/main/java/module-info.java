d59f29550aab7ba09b98cc07ed308d19ede0dd76
Derrick Sperling <sperlingjr.cs@gmail.com>
2024-02-27 00:41:40 -0500

    Finished Assignment

--- CSC325JavaFxAssign/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.csc325javafxassign {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.csc325javafxassign to javafx.fxml;
-    exports org.example.csc325javafxassign;
-}
